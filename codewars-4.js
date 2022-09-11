ae86f06ac29ae42a8911e5c8bbe746f9ba1fb4fa
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-11 12:55:13 +0400

    Categorize New Member

--- codewars-4.js
+++ codewars-4.js
@@ -6,3 +6,22 @@ function boolToWord(bool) {
   return bool ? 'Yes' : 'No';
 }
 console.log(boolToWord(false));
+
+// 42
+// Categorize New Member
+// https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/javascript
+
+function openOrSenior(data) {
+  return data.map((member) =>
+    member[0] >= 55 && member[1] > 7 ? 'Senior' : 'Open'
+  );
+}
+
+console.log(
+  openOrSenior([
+    [3, 12],
+    [55, 1],
+    [91, -2],
+    [53, 23],
+  ])
+);
