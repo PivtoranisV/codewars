a8e818bd7c36a444dc23080b248e672d346796a6
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-15 21:57:45 +0200

    Sentence Smash

--- codewars-4.js
+++ codewars-4.js
@@ -25,3 +25,11 @@ console.log(
     [53, 23],
   ])
 );
+
+// 43
+// Sentence Smash
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+function smash(words) {
+  return words.join(' ');
+}
+console.log(smash(['hello', 'amazing', 'world']));
