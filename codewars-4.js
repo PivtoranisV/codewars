3ea7ccbc4bb175fcc50889a5a84499c29dec6925
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-19 15:20:50 +0200

    Remove First and Last Character

--- codewars-4.js
+++ codewars-4.js
@@ -115,3 +115,12 @@ function rentalCarCost(d) {
 }
 
 console.log(rentalCarCost(7));
+
+// 49
+// Remove First and Last Character
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/javascript
+function removeChar(str) {
+  return str.slice(1, -1);
+}
+
+console.log(removeChar('eloquent'));
