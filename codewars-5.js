2fed0884e4da55d90643a3cf5dab43f2e6065f5a
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-09-23 23:55:23 +0400

    Grasshopper - Grade book

--- codewars-5.js
+++ codewars-5.js
@@ -33,3 +33,21 @@ function isIsogram(str) {
 }
 
 console.log(isIsogram('Dermatoglyphics'));
+
+// 53
+// Grasshopper - Grade book
+// https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/javascript
+
+function getGrade(s1, s2, s3) {
+  const averageScore = [s1, s2, s3].reduce((prev, next) => prev + next) / 3;
+  if (averageScore >= 90) {
+    return 'A';
+  } else if (averageScore >= 80 && averageScore < 90) {
+    return 'B';
+  } else if (averageScore >= 70 && averageScore < 80) {
+    return 'C';
+  } else if (averageScore >= 60 && averageScore < 70) {
+    return 'D';
+  } else return 'F';
+}
+console.log(getGrade(52, 8, 87));
