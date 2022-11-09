4a10c76fe4a1560a954dcb559a8f54a1a197cc38
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-11-09 12:41:38 +0400

    Tasks from SoftServe

--- SoftSeve-test.js
+++ SoftSeve-test.js
@@ -445,3 +445,141 @@ pizza.deleteIngredient(pizzaMenu.INGREDIENT_TOMATOES);
 console.log('getIngredients:', pizza.getIngredients());
 console.log('calculatePrice:', pizza.calculatePrice());
 console.log('calculateCalories:', pizza.calculateCalories());
+
+// Implement the getPromise(delay, message) function, which takes an integer number delay  (between 0 and 2000) and string message and returns a Promise that waits for specified amount of time (using delay argument) and resolves with the message.
+function getPromise(delay, message) {
+  return new Promise((resolve) => {
+    resolve(message);
+    setTimeout(resolve, delay);
+  });
+}
+
+getPromise(2000, 'hello').then(function (data) {
+  console.log(data);
+});
+
+// Write an add(x, y) function that takes two arguments x and y. The function should return a Promise that resolves with the sum of the two arguments if they are Numbers, or rejects with the message "Error!" otherwise.
+function add(x, y) {
+  return new Promise((resolve, reject) =>
+    typeof x === 'number' && typeof y === 'number'
+      ? resolve(x + y)
+      : reject('Error!')
+  );
+}
+
+add(2, 2).then((res) => console.log(res));
+
+// Implement the getAge() function to get user age. To find his age you need to call a getUser() async function that return a user object in format {role: "somerole", id: 1}. To get the actual user info you need to call another function getUserProfile(id), that uses id returned from the previous function and return user info as an object
+// {name: "Petro", age: 15}. The getAge() must return the age of the user.
+
+async function getUser() {
+  return { role: 'somerole', id: 1 };
+}
+
+async function getUserProfile(id) {
+  return { name: 'Petro', age: 15 };
+}
+
+async function getAge() {
+  const response = await getUser();
+  const id = response.id;
+  const user = await getUserProfile(id);
+  return user.age;
+}
+getAge().then((res) => console.log(res));
+
+// Implement the take() function that converts a sequence of iterated values into a sequence of length n.
+// Example usage:
+// const arr = ['a', 'b', 'c', 'd'];
+// for (const x of take(2, arr)) {
+// console.log(x);
+// }
+// Output:
+// a
+// b
+function* take(n, iterable) {
+  for (let i = 0; i < n; i++) {
+    yield iterable[i];
+  }
+}
+
+const generator = take(3, ['a', 'b', 'c', 'd']);
+const value = [...generator];
+
+console.log(value);
+
+// Please, implement a function accountPatients that takes a count of free beds in a hospital and returns two functions:
+// the first one for adding a patient
+// the second one for discharging a patient
+// accountPatients should keep track of free beds in a hospital and every time when a patient is admitted or discharged, print the count to the console like in examples:
+// A patient was discharged, 54 beds are available
+// A patient was admitted, 34 beds are available
+// When there are no beds available,
+// Can not admit a patient, no beds available should be printed
+// When there is an attempt to discharge a patient when there are no patients,
+// There are no patients to discharge should be printed
+
+const accountPatients = (beds) => {
+  let freeBeds = beds;
+
+  function admit() {
+    if (freeBeds > 0) {
+      freeBeds -= 1;
+      console.log(`A patient was admitted, ${freeBeds} beds are available`);
+    } else {
+      console.log('Can not admit a patient, no beds available');
+    }
+  }
+
+  function discharge() {
+    if (freeBeds === beds) {
+      console.log('There are no patients to discharge');
+    } else {
+      freeBeds += 1;
+      console.log(`A patient was discharged, ${freeBeds} beds are available`);
+    }
+  }
+
+  return [admit, discharge];
+};
+
+const [admit, discharge] = accountPatients(100);
+admit();
+admit();
+discharge();
+discharge();
+discharge();
+
+// Write a checkAdult(age) function whose input parameter is the age of the user age. The function checks whether the set age parameter is set correctly, if it is set incorrectly, the corresponding error should be generated and displayed in the console:
+// - if the age value has not been set, you need to create the following error: "Please, enter your age",
+// - If you set a negative age value, you need to create the following error: "Please, enter positive number",
+// - if a non-numeric value of age was specified, you need to create the following error: "Please, enter number",
+// - if the integer value of age was not specified, you need to create the following error: "Please, enter Integer number",
+// - If the user is under 18, you need to create the following error: "Access denied - you are too young!".
+// If there is no error, the message “Access allowed” is displayed in the console.
+// In the function, implement the handling of possible exceptions, providing the output to the console of the name and description of the error.
+// Regardless of whether the age parameter was set correctly or incorrectly, the message “Age verification complete” should be displayed at the end of the test.
+
+function checkAdult(age) {
+  try {
+    if (!age) {
+      throw new Error('Error Please, enter your age');
+    } else if (age < 0) {
+      throw new Error('Error Please, enter positive number');
+    } else if (typeof age !== 'number') {
+      throw new Error('Error Please, enter number');
+    } else if (age % 1 !== 0) {
+      throw new Error('Error Please, enter Integer number');
+    } else if (age < 18) {
+      throw new Error('Error Access denied - you are too young!');
+    } else {
+      console.log('Access allowed');
+    }
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    console.log('Age verification complete');
+  }
+}
+
+checkAdult(2);
